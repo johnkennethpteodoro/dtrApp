a178db8e4fc1256a80002258af5a25643581da39
chods <johnkennethpteodoro@gmail.com>
2025-04-10 15:29:45 +0800

    update the login ui

--- app/components/LeaveCredits.tsx
+++ app/components/LeaveCredits.tsx
@@ -11,27 +11,7 @@ function LeaveCredits() {
 					</button>
 				</div>
 
-				<div className="w-full bg-white rounded-b-lg px-8 pt-5">
-					<h1 className="text-center mb-4 text-gray-400">Leaves</h1>
-					<div className="flex justify-between border-b border-black mb-2">
-						<h1>Vacation</h1> <h1>7</h1>
-					</div>
-					<div className="flex justify-between pt-3 border-b border-black mb-2">
-						<h1>Sick</h1> <h1>5</h1>
-					</div>
-					<div className="flex justify-between pt-3 border-b border-black mb-2">
-						<h1>Berreavement</h1> <h1>3</h1>
-					</div>
-					<div className="flex justify-between pt-3 border-b border-black mb-2">
-						<h1>Emergency Leave</h1> <h1>3</h1>
-					</div>
-					<div className="flex justify-between pt-3 border-b border-black mb-2">
-						<h1>Offset Leave</h1> <h1>0</h1>
-					</div>
-					<div className="flex justify-between pt-3 pb-9">
-						<h1>Compensatory Time Off</h1> <h1>0</h1>
-					</div>
-				</div>
+				<div className="w-full bg-white rounded-b-lg px-8 pt-5 h-36"></div>
 			</div>
 		</div>
 	);
--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -50,6 +50,7 @@ function Login() {
 
 	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
+		console.log("Login button clicked");
 		if (loginAttempts >= 3) {
 			alert("Maximum limit of 3 invalid password attempts reached. Please try again later.");
 			return;
@@ -59,15 +60,15 @@ function Login() {
 
 	return (
 		<div className="w-[400px]">
-			<div className="bg-white px-7 py-5 rounded-md ">
+			<div className="bg-white px-7 py-5 rounded-xl">
 				<div className="flex flex-col items-center justify-center">
-					<div className="mt-8 mb-4">
+					<div className="mt-8 mb-2">
 						<Clock color="black" size={48} />
 					</div>
 					<h1 className="text-xl font-bold mb-6">TimeAxis</h1>
 				</div>
 
-				<form onSubmit={handleSubmit} className="pb-20">
+				<form onSubmit={handleSubmit} className="pb-16">
 					<div>
 						<label htmlFor="email" className="block mb-1 text-sm font-medium">
 							Email
@@ -77,10 +78,12 @@ function Login() {
 							placeholder="Username or Email"
 							value={email}
 							onChange={handleEmailChange}
-							className="border border-gray-200 py-2 px-3 rounded-sm w-full text-sm"
+							className="border border-gray-200 py-2 px-3 rounded-sm w-full text-sm focus:outline-none"
 						/>
 						{emailError && (
-							<p style={{ color: "red", marginTop: "5px" }}>{emailError}</p>
+							<p className="text-sm" style={{ color: "red", marginTop: "5px" }}>
+								{emailError}
+							</p>
 						)}
 
 						<label htmlFor="password" className="block mt-4 mb-1 text-sm font-medium">
@@ -91,27 +94,35 @@ function Login() {
 							placeholder="Password"
 							value={password}
 							onChange={handlePasswordChange}
-							className="border border-gray-200 py-2 px-3 rounded-sm w-full text-sm"
+							className="border border-gray-200 py-2 px-3 focus:outline-none rounded-sm w-full text-sm"
 						/>
 						{passwordError && (
-							<p style={{ color: "red", marginTop: "5px" }}>{passwordError}</p>
+							<p className="text-sm" style={{ color: "red", marginTop: "5px" }}>
+								{passwordError}
+							</p>
 						)}
 					</div>
 
 					<button
 						type="submit"
 						disabled={loginDisabled}
-						className="mt-8 w-full text-center bg-blue-700 text-white rounded-lg py-2 font-medium"
+						className="mt-8 w-full text-center bg-zinc-950 text-white rounded-lg py-2 font-medium "
 					>
 						Login
 					</button>
+					{error && (
+						<p className="mt-2 text-center text-sm" style={{ color: "red" }}>
+							{error}
+						</p>
+					)}
+
+					{loginAttempts >= 3 && (
+						<p className="mt-2 text-center text-sm" style={{ color: "red" }}>
+							Maximum limit of invalid password attempts reached. Please try again
+							later.
+						</p>
+					)}
 				</form>
-				{error && <p style={{ color: "red" }}>{error}</p>}
-				{loginAttempts >= 3 && (
-					<p style={{ color: "red" }}>
-						Maximum limit of invalid password attempts reached. Please try again later.
-					</p>
-				)}
 			</div>
 		</div>
 	);
